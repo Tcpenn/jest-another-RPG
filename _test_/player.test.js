479b90f12aeb85c2266d1a099e41fbc0bd0f7b6e
Tcpenn <tcpenn1026@gmail.com>
2021-09-27 19:23:34 -0400

    added player object and other tests for health and inventory

--- _test_/player.test.js
+++ _test_/player.test.js
@@ -5,18 +5,16 @@ jest.mock('../lib/Potion');
 
 console.log(new Potion());
 
-test('create a player object', () => {
+test('creates a player object', () => {
     const player = new Player('Dave');
-
-
-expect(player.name).toBe('Dave');
-expect(player.health).toEqual(expect.any(Number));
-expect(player.strength).toEqual(expect.any(Number));
-expect(player.agility).toEqual(expect.any(Number));
-expect(player.inventory).toEqual(
-    expect.arrayContaining([expect.any(object)])
-)
-});
+  
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+  
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+  });
 
 test('gets player stats as an object', () => {
     const player = new Player('Dave');
@@ -36,3 +34,40 @@ test('gets inventory from player or returns false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+test ('gets players health value', ()=> {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()))
+});
+
+test('checks to see if the player is alive', ()=> {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
+
+  test ("gets player's attack value", () => {
+      const player = new Player('Dave');
+      player.strength = 10;
+
+      expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+      expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+  });
